cb6079ede149dc8f74b4dabd6ace4d7f42993234
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-30 11:18:33 -0500

    ADDED DO WHILE LOOPS FOR GAME FUNCTIONALITY. GAME IS WORKING.

--- highlow.php
+++ highlow.php
@@ -1,13 +1,33 @@
 <?php 
-
+// INSTRUCTIONS 
 echo "----------------------------------------------------" .PHP_EOL;
 echo "-------------WELOME TO MY HIGH/LOW GAME-------------" .PHP_EOL;
 echo "----------------------------------------------------" .PHP_EOL;
-echo "A random number will be generated between 1 and 100" .PHP_EOL;
-echo "You try to guess the number. Based on your response" .PHP_EOL;
-echo "you will be told if the number is HIGHER or LOWER." .PHP_EOL;
-echo "Keep entering a number until you get the right one!" .PHP_EOL;
- 
+echo "A random number will be generated between 1 and 100 " .PHP_EOL;
+echo "You try to guess the number. Based on your response " .PHP_EOL;
+echo "you will be told if the number is HIGHER or LOWER.  " .PHP_EOL;
+echo "Keep entering a number until you get the right one! " .PHP_EOL;
+echo "Use CTRL-C to exit game, if you are a quitter."		.PHP_EOL;
+echo "----------------------------------------------------" .PHP_EOL; 
+
+$randomNumber = mt_rand(1,100); // RANDOM NUMBER GENERATOR BETWEEN 1 AND 100
+fwrite(STDOUT, 'Guess a number between 1 and 100: ') .PHP_EOL;
+
+do {
+	echo "Guess? ";
+	$userGuess = trim(fgets(STDIN));
+	// $numberOfTries = array($userGuess);
+	if($userGuess > $randomNumber) {
+	echo "Lower".PHP_EOL;
+	} else if($userGuess < $randomNumber) {
+	echo "Higher" .PHP_EOL;
+	} else if($userGuess == $randomNumber) {
+	echo "Good guess! The number is $randomNumber!" .PHP_EOL;
+	} 
+} while($userGuess != $randomNumber);
+
+
+		
 
 
 
