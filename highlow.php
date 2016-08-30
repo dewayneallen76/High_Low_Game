a10d383ae4c6caca39331c1e167eefd4467e1cd7
Dewayne Allen <dewayneallen76@gmail.com>
2016-08-30 10:12:42 -0500

    ADDED INSTRUCTIONS

--- highlow.php
+++ highlow.php
@@ -1,7 +1,13 @@
 <?php 
 
-
-
+echo "----------------------------------------------------" .PHP_EOL;
+echo "-------------WELOME TO MY HIGH/LOW GAME-------------" .PHP_EOL;
+echo "----------------------------------------------------" .PHP_EOL;
+echo "A random number will be generated between 1 and 100" .PHP_EOL;
+echo "You try to guess the number. Based on your response" .PHP_EOL;
+echo "you will be told if the number is HIGHER or LOWER." .PHP_EOL;
+echo "Keep entering a number until you get the right one!" .PHP_EOL;
+ 
 
 
 
